ade195d44cfa5aef773fbb6ccc2aa250d281e96b
Martin <martingonzalez2022@udec.cl>
2024-11-02 20:26:22 -0300

    feat: Centrar la ventana al iniciar la aplicación

--- src/Ventana.py
+++ src/Ventana.py
@@ -1,8 +1,10 @@
 import pygame
+import os
 
 
 class Ventana:
     def __init__(self, rows, cols, MARGIN, CELDA_SIZE):
+        os.environ['SDL_VIDEO_CENTERED'] = '1'  # Establece la variable de entorno para centrar la ventana
         pygame.init()
         self.crearVentana(rows, cols, MARGIN, CELDA_SIZE)
 
@@ -16,6 +18,7 @@ class Ventana:
         return (ancho, alto)
         
     def crearVentana(self, rows, cols, MARGIN, CELDA_SIZE):
+
         self.screen = pygame.display.set_mode(self.calcularTamañoVentana(rows, cols, MARGIN, CELDA_SIZE))
         pygame.display.set_caption("Nonograma")
         return self.screen
